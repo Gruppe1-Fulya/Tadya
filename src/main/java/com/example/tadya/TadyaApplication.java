30d8af8c2a9b93459c0cf1d86ed70251d318d5c6
yigitalcks <yigitalkcs0@outlook.com>
2023-05-04 10:47:40 +0300

    vatana millete hayırlı olsun

---
+++ src/main/java/com/example/tadya/TadyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tadya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tadya/TadyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tadya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
