9c3129d923a939127f7b1a235b3a2ddd5f1f874e
yigitalcks <yigitalkcs0@outlook.com>
2023-05-04 13:57:48 +0300

    start

--- src/main/java/com/example/tadya/BuildingController.java
+++ src/main/java/com/example/tadya/BuildingController.java
@@ -3,12 +3,12 @@ package com.example.tadya;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-/*
+
 @RestController
 public class BuildingController {
-    @RequestMapping(method = RequestMethod.GET, path = "/index")
+    @RequestMapping(method = RequestMethod.GET, path = "/index.html")
     public String getIndex() {
         return "index";
     }
 }
-*/
+
