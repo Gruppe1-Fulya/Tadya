8015516a54634e7ab8dea6d6b38d1fee40074bc4
yigitalcks <yigitalkcs0@outlook.com>
2023-05-05 10:00:13 +0300

    yanlış yazmışım

--- src/main/java/com/example/tadya/Bericht.java
+++ src/main/java/com/example/tadya/Bericht.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class Bericht {
     enum Zustand { // Levels of damage
-        L0, // Intakt
+        L0, // Intact
         L1, // Slightly damaged
         L2, // Severely damaged
         L3 // Collapsed
