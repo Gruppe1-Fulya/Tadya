7d270af2afc3029dcb31195c490ebd6e1fad6e82
yigitalcks <yigitalkcs0@outlook.com>
2023-06-02 00:21:25 +0300

    add building

--- src/main/java/com/example/tadya/controller/GebeaudeController.java
+++ src/main/java/com/example/tadya/controller/GebeaudeController.java
@@ -4,10 +4,7 @@ import com.example.tadya.model.Gebeaude;
 import com.example.tadya.repository.GebeaudeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -28,7 +25,12 @@ public class GebeaudeController {
     }
     @GetMapping("/{osm_id}")
     public Gebeaude findById(@PathVariable String osm_id) {
-        return repository.findById(osm_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "bina bukunamadi"));
+        return repository.findById(osm_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Bina Bulunamadı"));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void sendHelpRequest(@RequestBody Gebeaude gebeaude) {
+        repository.addBuilding(gebeaude);
+    }
 }
