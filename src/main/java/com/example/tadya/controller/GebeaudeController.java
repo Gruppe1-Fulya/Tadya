d3c566c55021ac582c99a796b6e8484665f192e2
yigitalcks <yigitalkcs0@outlook.com>
2023-06-02 17:31:11 +0300

    controller

--- src/main/java/com/example/tadya/controller/GebeaudeController.java
+++ src/main/java/com/example/tadya/controller/GebeaudeController.java
@@ -2,35 +2,51 @@ package com.example.tadya.controller;
 
 import com.example.tadya.model.Gebeaude;
 import com.example.tadya.repository.GebeaudeRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/map")
 public class GebeaudeController {
-
-    private final GebeaudeRepository repository;
-
-    public GebeaudeController(GebeaudeRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private GebeaudeRepository repository;
 
     @GetMapping
     public List<Gebeaude> findAll() {
         return repository.findAll();
     }
     @GetMapping("/{osm_id}")
-    public Gebeaude findById(@PathVariable String osm_id) {
-        return repository.findById(osm_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Bina Bulunamadı"));
+    public Gebeaude findById(@PathVariable Integer osm_id) {
+        return repository.findById(osm_id);
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add/{osm_id}")
+    public void add_test(@RequestParam(required = false) @PathVariable Integer osm_id) {
+
+        Gebeaude g = new Gebeaude(
+                osm_id, "building",
+                LocalDateTime.now(),
+                false,
+                null,
+                null
+        );
+        repository.save(g);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void sendHelpRequest(@RequestBody Gebeaude gebeaude) {
-        repository.addBuilding(gebeaude);
+    public void sendHelpRequest(@Valid @RequestBody Gebeaude gebeaude) {
+        repository.save(gebeaude);
+    }
+
+    @DeleteMapping("/{osm_id}")
+    public void deleteBuilding(@PathVariable Integer osm_id) {
+        repository.deleteById(osm_id);
     }
 }
---
+++ src/main/java/com/example/tadya/repository/JdbcGebeaudeRepository.java
@@ -0,0 +1,72 @@
+package com.example.tadya.repository;
+
+import com.example.tadya.model.Gebeaude;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class JdbcGebeaudeRepository implements GebeaudeRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static Gebeaude mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Gebeaude(rs.getInt("osm_id"),
+                rs.getString("fclass"),
+                rs.getObject("mel_zeit", LocalDateTime.class),
+                rs.getBoolean("besteatigung"),
+                rs.getInt("bericht_id"),
+                rs.getInt("auto_id"));
+    }
+    @Override
+    public void save(Gebeaude building) {
+        jdbcTemplate.update("INSERT INTO gebeaude (osm_id, mel_zeit, fclass, besteatigung, bericht_id, auto_id) VALUES(?,?,?,?,?,?)", building.osm_id(),
+                LocalDateTime.now(), building.fclass(), building.besteatigung(), building.bericht_id(), building.auto_id());
+    }
+    /*
+    @Override
+    public int update(Tutorial tutorial) {
+        return jdbcTemplate.update("UPDATE tutorials SET title=?, description=?, published=? WHERE id=?",
+                new Object[] { tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished(), tutorial.getId() });
+    }
+    */
+    @Override
+    public Gebeaude findById(Integer id) {
+        try {
+            Gebeaude gebeaude = jdbcTemplate.queryForObject("SELECT * FROM gebeaude WHERE osm_id=?",
+                    JdbcGebeaudeRepository::mapRow, id);
+            return gebeaude;
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public int deleteById(Integer osm_id) {
+        return jdbcTemplate.update("DELETE FROM gebeaude WHERE osm_id=?", osm_id);
+    }
+
+    @Override
+    public List<Gebeaude> findAll() {
+        return jdbcTemplate.query("SELECT * from gebeaude", JdbcGebeaudeRepository::mapRow);
+    }
+    /*
+    @Override
+    public List<Gebeaude> findByPublished(boolean published) {
+        return jdbcTemplate.query("SELECT * from tutorials WHERE published=?",
+                JdbcGebeaudeRepository::mapRow, published);
+    }
+    */
+    @Override
+    public int deleteAll() {
+        return jdbcTemplate.update("DELETE from gebeaude");
+    }
+
+}
